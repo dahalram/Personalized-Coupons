a89744565bba1f250f69510531735fb7610cfd9b
Ram Hari Dahal <rambo@Rams-MacBook-Pro.local>
2018-03-30 23:29:50 -0400

    Login pages and bank info

---
+++ PersonalizedCoupons/app/src/main/java/com/example/rambo/personalizedcoupons/BankLoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.rambo.personalizedcoupons;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BankLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank_login);
+
+        final Button btBofA = (Button) findViewById(R.id.btBofA);
+        final Button btWellsFargo = (Button) findViewById(R.id.btWellsFargo);
+
+        btBofA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bofaIntent = new Intent(BankLoginActivity.this, BankInfoLoginActivity.class);
+                BankLoginActivity.this.startActivity(bofaIntent);
+            }
+        });
+    }
+}
