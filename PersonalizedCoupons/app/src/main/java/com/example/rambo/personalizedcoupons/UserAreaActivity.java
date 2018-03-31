7c17ff2d4bae1e1f05caf30a00c5cc1b2a014bcc
ranjankhadka123 <ranjan.khadka@bison.howard.edu>
2018-03-31 03:51:55 -0400

    RecyclerView for coupon feeds.

--- PersonalizedCoupons/app/src/main/java/com/example/rambo/personalizedcoupons/UserAreaActivity.java
+++ PersonalizedCoupons/app/src/main/java/com/example/rambo/personalizedcoupons/UserAreaActivity.java
@@ -2,12 +2,51 @@ package com.example.rambo.personalizedcoupons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidviewhover.BlurLayout;
 
 public class UserAreaActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+    MyAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
+
+        BlurLayout sampleLayout = (BlurLayout)findViewById(R.id.sample);
+        View hover = LayoutInflater.from(this).inflate(R.layout.hover_view, null);
+        sampleLayout.setHoverView(hover);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = {"a", "b", "c"};
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
+
+
 }
+
